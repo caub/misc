089f2aa1e6400bf2fa72bf6f8cce6f147483ecab
caub <cyril.auburtin@gmail.com>
2019-03-22 23:58:05 +0100

    up promise-concurrent

--- utils/promise-concurrent.js
+++ utils/promise-concurrent.js
@@ -1,20 +1,16 @@
-const all = async (xs, f, n=Infinity) => {
-  const chunks = Array.from({length: Math.ceil(xs.length/n)||1}, (_,i)=>xs.slice(i*n, (i+1)*n));
-  for (const chunk of chunks) await Promise.all(chunk.map(f));
-}
-var delay=t=>new Promise(r=>setTimeout(r,t))
-console.time(1); await all([500,3000,800,2000,1500], delay, 2); console.timeEnd(1)
-// 1: 6501.330078125ms
-
-
-const findTruthyIndexes = (arr, n=Infinity) => { const a=[]; for (let i=0;i<arr.length && a.length<n;i++){ if (arr[i]) a.push(i); }; return a };
-const concurrent=async (xs, f, n=Infinity) => {
-  const rs = xs.map(() => true);
-  let a = xs.slice(0, n);
-  while (a.length) {
-    await Promise.race(a.map(i => f(xs[i]).then(() => {rs[i] = false;})));
-    a = findTruthyIndexes(rs);
-    }
+async function concurrent(xs, f, n=Infinity) {
+  const finished = Symbol();
+  let promises = xs.slice(0, n).map(f), others = xs.slice(n);
+  while (promises.length) {
+    await Promise.race(promises.map(promise => promise.then(() => { promise[finished] = true; })));
+    promises = promises.filter(promise => !promise[finished]);
+    const nResolved = n - promises.length;
+    promises.push(...others.slice(0, nResolved).map(f));
+    others = others.slice(nResolved);
+  }
 };
+var delay = t => new Promise(r=>setTimeout(r, t));
+(async()=>{
 console.time(1); await concurrent([500,3000,800,2000,1500], delay, 2); console.timeEnd(1)
-// 1: 5805.26513671875ms
+// 1: 4501.019775390625ms
+})()
