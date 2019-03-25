e0bd8bdb6898202d7f9232973cd9b6ab374f7c64
caub <cyril.auburtin@gmail.com>
2019-03-25 06:58:45 +0100

    promise-concurrent using thunks as arguments

--- utils/promise-concurrent.js
+++ utils/promise-concurrent.js
@@ -1,16 +1,22 @@
-async function concurrent(xs, f, n=Infinity) {
+/**
+ * 
+ * @param {array<() => Promise>} thunks 
+ * @param {number?} n 
+ */
+async function concurrent(thunks, n = Infinity) {
   const finished = Symbol();
-  let promises = xs.slice(0, n).map(f), others = xs.slice(n);
+  let promises = thunks.slice(0, n).map(thunk => thunk()), others = thunks.slice(n);
   while (promises.length) {
     await Promise.race(promises.map(promise => promise.then(() => { promise[finished] = true; })));
     promises = promises.filter(promise => !promise[finished]);
     const nResolved = n - promises.length;
-    promises.push(...others.slice(0, nResolved).map(f));
+    promises.push(...others.slice(0, nResolved).map(thunk => thunk()));
     others = others.slice(nResolved);
   }
-};
-var delay = t => new Promise(r=>setTimeout(r, t));
-(async()=>{
-console.time(1); await concurrent([500,3000,800,2000,1500], delay, 2); console.timeEnd(1)
-// 1: 4501.019775390625ms
+}
+
+const delay = t => new Promise(r => setTimeout(r, t));
+(async () => {
+  console.time(1); await concurrent([500, 3000, 800, 2000, 1500].map(t => () => delay(t)), 2); console.timeEnd(1)
+  // 1: 4501.019775390625ms
 })()
