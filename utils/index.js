8edae297818aff8d82d26795231182eff34b0cc1
Cyril Auburtin <cyril.auburtin@gmail.com>
2022-11-09 17:32:30 +0100

    Update index.js

--- utils/index.js
+++ utils/index.js
@@ -1,16 +1,22 @@
 export function flat(o, p=[]){
-  // -- using lodash
+  return Object.fromEntries(
+    Object.entries(nestedObj).flatMap(([k, v]) => {
+      if (v && typeof v === 'object') {
+        const subObj = flat(v, [...p, k]);
+        return Object.entries(subObj);
+      }
+      return [[[...p, k].join(sep), v]];
+    })
+  );
+}
+
+export function unflat(o){
+    // -- using lodash
 //   const o = {};
 //   for (const [k, v] of Object.entries(flatObj)) {
 //     _.set(o, k.replaceAll(sep, '.'), v);
 //   }
 //   return o;
-  return Object.fromEntries(
-    Object.entries(o).flatMap(([k, v]) => typeof v=='string' ? [[[...p, k].join('.'),v]] : Object.entries(flat(v, [...p, k])))
-  );
-}
-
-export function unflat(o){
   return Object.entries(o).reduce((a, [k, v]) => {
     const ks = k.split('.');
     return {
